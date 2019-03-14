7814ec7c21f127ca40f32d1a4eecef510bb4a115
Roberto D'Auria <roberto@measurementlab.net>
2019-03-14 12:47:32 +0100

    Move mockClient into test function and improve readability.

--- storage/datastore_test.go
+++ storage/datastore_test.go
@@ -8,14 +8,14 @@ import (
 	"cloud.google.com/go/datastore"
 )
 
-// DatastoreClientMock is a fake DatastoreClient for testing.
-type DatastoreClientMock struct {
+// MockDatastoreClient is a fake DatastoreClient for testing.
+type MockDatastoreClient struct {
 	Creds      []*Credentials
 	mustFail   bool
 	skipAppend bool
 }
 
-func (d DatastoreClientMock) GetAll(ctx context.Context, q *datastore.Query,
+func (d MockDatastoreClient) GetAll(ctx context.Context, q *datastore.Query,
 	dst interface{}) ([]*datastore.Key, error) {
 
 	if d.mustFail {
@@ -46,37 +46,37 @@ var fakeDrac = &Credentials{
 	Address:  testAddress,
 }
 
-var mockClient = DatastoreClientMock{
-	Creds: []*Credentials{
-		fakeDrac,
-	},
-}
-
 func TestFindCredentials(t *testing.T) {
 	ctx := context.Background()
+	var mockClient = MockDatastoreClient{
+		Creds: []*Credentials{
+			fakeDrac,
+		},
+	}
 
+	// FindCredentials must return valid credentials for a known hostname.
 	creds, err := FindCredentials(ctx, mockClient, testHost)
 	if err != nil {
 		t.Errorf("FindCredentials() error = %v", err)
 		return
 	}
-
 	if *creds != *fakeDrac {
 		t.Errorf("FindCredentials() didn't return a valid Credentials.")
 	}
 
+	// FindCredentials must fail if there is an error while retrieving
+	// credentials from Datastore.
 	mockClient.mustFail = true
 	_, err = FindCredentials(ctx, mockClient, testHost)
-
 	if err == nil {
 		t.Errorf("FindCredentials() didn't return an error as expected.")
 	}
 
+	// FindCredentials must return an error if there is no result for the
+	// requested hostname.
 	mockClient.mustFail = false
 	mockClient.skipAppend = true
-
 	_, err = FindCredentials(ctx, mockClient, testHost)
-
 	if err == nil {
 		t.Errorf("FindCredentials() didn't return an error as expected.")
 	}
