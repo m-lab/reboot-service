23620032234791bbcb4732e3932d8f851e83a18c
Roberto D'Auria <roberto@measurementlab.net>
2019-03-24 22:21:40 +0100

    Add package "services" with function RebootDRAC.

---
+++ services/reboot.go
@@ -0,0 +1,54 @@
+package services
+
+import (
+	"context"
+	"log"
+
+	"github.com/m-lab/reboot-service/drac"
+
+	"cloud.google.com/go/datastore"
+	"github.com/m-lab/reboot-service/storage"
+)
+
+var dsNewClient = datastore.NewClient
+
+const (
+	projectID        = "mlab-sandbox"
+	defaultNamespace = "reboot-api"
+)
+
+func retrieveDRACCredentials(ctx context.Context, host string) (*storage.Credentials, error) {
+	client, err := dsNewClient(ctx, projectID)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return storage.FindCredentials(ctx, storage.Datastore{
+		Client:    client,
+		Namespace: defaultNamespace,
+	}, host)
+}
+
+// RebootDRAC reboots a node via its DRAC.
+func RebootDRAC(ctx context.Context, host string, port int32) (string, error) {
+	cred, err := retrieveDRACCredentials(ctx, host)
+	if err != nil {
+		log.Printf("Cannot retrieve DRAC credentials: %v", err)
+		return "", err
+	}
+
+	conn, err := drac.NewConnection(host, port, cred.Username, cred.Password, "", &drac.DialerImpl{})
+	if err != nil {
+		log.Printf("Cannot initialize DRAC connection %v", err)
+		return "", err
+	}
+
+	output, err := conn.Reboot()
+	if err != nil {
+		log.Printf("Cannot send reboot command: %v", err)
+		return "", err
+	}
+
+	return output, nil
+}
